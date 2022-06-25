bd33b64d84220850f082f6e2cb315ed6d4fca011
fryhsueh <fryhsueh@gmail.com>
2022-06-26 00:33:53 +0800

    Special Form: make_let_frame

--- projects/scheme/scheme.py
+++ projects/scheme/scheme.py
@@ -466,6 +466,14 @@ def make_let_frame(bindings, env):
     names, values = nil, nil
     # BEGIN PROBLEM 14
     "*** YOUR CODE HERE ***"
+    while bindings is not nil:
+        binding = bindings.first
+        validate_form(binding, 2, 2)
+      
+        names, values = Pair(binding.first, names), Pair(scheme_eval(binding.rest.first, env), values)
+        bindings = bindings.rest
+
+    validate_formals(names)
     # END PROBLEM 14
     return env.make_child_frame(names, values)
 
